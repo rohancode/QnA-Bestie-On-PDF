6b2bbecb020593ffeb8282e0fadced45fdcb4dca
Rohan Rathore <rohan.rathore93@gmail.com>
2023-08-26 18:52:07 +0530

    changes

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
-api_key = os.getenv("aaa")
-print(api_key)
-print("Hello World")
+
+# Access environment variables
+api_key = os.getenv("API_KEY")
+database_url = os.getenv("DATABASE_URL")
+debug_mode = os.getenv("DEBUG")
+
+# Print the values
+print("API Key:", api_key)
+print("Database URL:", database_url)
+print("Debug Mode:", debug_mode)
